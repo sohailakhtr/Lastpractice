a0702c27ac87383fe5585696d87f116816c5888c
sohailakhtr <soh7132s@gmail.com>
2024-10-10 10:54:11 +0500

    master code commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
+void junierfun();
+void senierfun();
+
 int main()
 {
-    cout<<"hellow world";
+    cout<<"hellow world"<<endl;
+    cout<<"junierr code "<<endl;
+    junierfun();
+    cout<<"senier"<<endl;
+    senierfun();
     return 0;
+    
 } // namespace std;
